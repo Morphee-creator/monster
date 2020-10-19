2def1ed29c72e1c58b3add6a160b1b36f5b02575
Moprhee-creator <milican.2405.student@ifosupwavre.be>
2020-10-19 16:13:34 +0200

    Updates

---
+++ precache-manifest.69da1ce102ec64d3682cd5e2b9b6f006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dad10abf6020e22a462a963aeafce77",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "6eb6088042ff9be7a279",
+    "url": "/monster/static/css/main.ecccbd5e.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monster/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb6088042ff9be7a279",
+    "url": "/monster/static/js/main.96c6a34e.chunk.js"
+  },
+  {
+    "revision": "5446ca17d6312361b3b2",
+    "url": "/monster/static/js/runtime-main.b7e78b55.js"
+  }
+]);
